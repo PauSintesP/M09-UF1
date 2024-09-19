cfafc31d567809d59c0d938579bf35ed2b20888d
PauSintesP <pausintespaul@gmail.com>
2024-09-19 19:00:41 +0200

    activitat inicial

---
+++ Rot13.java
@@ -0,0 +1,12 @@
+public class Rot13 {
+    public char[] lletresMin = new int['a','b','c','d','e','f','g','h','i','j','k','l','m',''];
+    public String xifraRot13(String text) {
+
+        retur 
+    }
+
+    public String desxifraRot13(String text){
+
+    }
+}
+
